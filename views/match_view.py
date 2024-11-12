e6c74e2777269e1e149e2006dabe3c03c5e7094a
Laureopen <laurebretagne56@orange.fr>
2024-11-12 12:48:37 +0100

    mise à jour match

---
+++ views/match_view.py
@@ -0,0 +1,4 @@
+# Exemple de fonction d'affichage pour les matchs
+def display_match_results(round_matches):
+    for i, match in enumerate(round_matches, start=1):
+        print(f"Match {i}: {match}")
