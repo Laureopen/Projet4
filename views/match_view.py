c4c1a1c33776add88c8c6a8030476ba91688a163
Laureopen <laurebretagne56@orange.fr>
2024-11-21 14:21:33 +0100

    modif match view

--- views/match_view.py
+++ views/match_view.py
@@ -14,8 +14,8 @@ class MatchView:
     def display_match_details(self, match):
         """Affiche les détails d'un match particulier (joueurs, score, etc.)."""
         print(f"\nDétails du match :")
-        print(f"Joueur 1: {match.player1.name} vs Joueur 2: {match.player2.name}")
-        print(f"Score : {match.score1} - {match.score2}")
-        winner = match.get_winner()  # Méthode fictive pour obtenir le gagnant
-        print(f"Gagnant : {winner.name}")
+        print(f"Joueur 1: {match.match_info[0][0].last_name} {match.match_info[0][0].first_name} | Score: {match.match_info[0][1]}")
+        print(f"Joueur 2: {match.match_info[1][0].last_name} {match.match_info[1][0].first_name} | Score: {match.match_info[1][1]}")
+        winner = match.match_info[0][1] > match.match_info[1][1] and match.match_info[0][0] or match.match_info[1][0]
+        print(f"Gagnant: {winner.last_name} {winner.first_name}")
 
