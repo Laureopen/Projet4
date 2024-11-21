a20cb8dbae7ffd855d6766a14b53d9bfbf0ad9c0
Laureopen <laurebretagne56@orange.fr>
2024-11-21 14:20:24 +0100

    modif player view

--- views/player_view.py
+++ views/player_view.py
@@ -1,6 +1,4 @@
 class PlayerView:
-    """Vue pour les interactions liées aux joueurs."""
-
     def prompt_for_new_player(self):
         """Invite pour créer un nouveau joueur."""
         print("Ajout d'un nouveau joueur :")
@@ -15,19 +13,5 @@ class PlayerView:
 
     def show_player_added(self, player):
         """Afficher une confirmation qu'un joueur a été ajouté."""
-        print(f"Joueur {player.last_name} {player.first_name} ajouté avec succès !")
+        print(f"Joueur {player.first_name} {player.last_name} ajouté avec succès !")
 
-    def display_tournament_results(self, tournament):#tournament
-        """Afficher les résultats finaux d'un tournoi."""
-        print(f"\nRésultats finaux pour le tournoi {tournament.name} :")
-        sorted_players = sorted(tournament.players, key=lambda x: x[1], reverse=True)
-        for player_info in sorted_players:
-            player, points, opponents = player_info
-            opponents_names = ", ".join([opponent.last_name for opponent in opponents])
-            print(f"{player.last_name} {player.first_name}: {points} points, a rencontré {opponents_names}")
-
-    def prompt_for_continue(self): #menu-view
-        """Demander à l'utilisateur s'il souhaite continuer."""
-        print("Souhaitez-vous continuer ?")
-        choice = input("Y/n: ").lower()
-        return choice != "n"
