56cee4b804ff24dfbf5da0bc18f80f5da91974ad
Laureopen <laurebretagne56@orange.fr>
2024-11-22 16:20:39 +0100

    modif player.py

--- models/player.py
+++ models/player.py
@@ -1,20 +1,17 @@
 from datetime import datetime
 
 class Player:
-    def __init__(self, last_name, first_name, birth_date, national_id):
+    def __init__(self, last_name, first_name, birth_date, player_id):
         self.last_name = last_name
         self.first_name = first_name
-        self.birth_date = datetime.strptime(birth_date, "%Y-%m-%d")  # Format YYYY-MM-DD
-        self.national_id = national_id
+        self.birth_date = datetime.strptime(birth_date, '%Y-%m-%d') if isinstance(birth_date, str) else birth_date
+        self.player_id = player_id
 
     def to_dict(self):
-        """Retourne un dictionnaire représentant le joueur pour JSON."""
+        """Retourne un dictionnaire pour le joueur."""
         return {
-            "last_name": self.last_name,
-            "first_name": self.first_name,
-            "birthdate": self.birth_date.strftime('%Y-%m-%d'),  # Convertir en chaîne au format YYYY-MM-DD
-            "player_id": self.national_id  # Correction de 'player_id' vers 'national_id'
+            'last_name': self.last_name,
+            'first_name': self.first_name,
+            'birth_date': self.birth_date.strftime('%Y-%m-%d'),
+            'player_id': self.player_id
         }
-
-    def __str__(self):
-        return f"{self.last_name} {self.first_name}, Date de Naissance: {self.birth_date.strftime('%d-%m-%Y')}, ID: {self.national_id}"
