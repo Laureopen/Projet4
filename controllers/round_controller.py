0bba5c32dd67cdc6f2a4c7b99304b3498a2b8abe
Laureopen <laurebretagne56@orange.fr>
2024-12-12 14:12:57 +0100

    Mise à jour

--- controllers/round_controller.py
+++ controllers/round_controller.py
@@ -2,34 +2,44 @@ from controllers.match_controller import create_match
 from controllers.player_controller import have_played_together
 from models.round import Round
 
-def RoundController(tournament, round_num, start_date, end_date):
-    players = sorted(tournament.players, key=lambda x: x[1], reverse=True)  # Trier les joueurs par score
-    round_matches = []
-    available_players = players.copy()
+class RoundController:
+    def __init__(self, tournament, round_num, start_date, end_date):
+        self.tournament = tournament
+        self.round_num = round_num
+        self.start_date = start_date
+        self.end_date = end_date
 
-    # Créer un objet Round
-    current_round = Round(round_num, start_date, end_date)
+    def create_round(self):
+        players = sorted(self.tournament.players, key=lambda x: x[1], reverse=True)  # Trier les joueurs par score
+        round_matches = []
+        available_players = players.copy()
 
-    # Création des matchs pour ce round
-    while available_players:
-        player1 = available_players.pop(0)
-        player2 = None
+        # Créer un objet Round
+        current_round = Round(self.round_num, self.start_date, self.end_date)
 
-        for idx, candidate in enumerate(available_players):
-            if not have_played_together(player1, candidate):
-                player2 = available_players.pop(idx)
-                break
+        # Création des matchs pour ce round
+        while available_players:
+            player1 = available_players.pop(0)
+            player2 = None
 
-        if player2 is None:
-            player2 = available_players.pop(0)
+            for idx, candidate in enumerate(available_players):
+                if not have_played_together(player1, candidate):
+                    player2 = available_players.pop(idx)
+                    break
 
-        match = create_match(player1[0], player2[0])  # Assurez-vous que create_match prend les bons arguments
-        round_matches.append(match)
-        current_round.add_match(match)  # Ajouter le match à l'objet Round
+            if player2 is None:
+                player2 = available_players.pop(0)
 
-        player1[1] += match.match_info[0][1]  # Mise à jour du score
-        player2[1] += match.match_info[1][1]
-        player1[2].append(player2)
-        player2[2].append(player1)
+            match = create_match(player1[0], player2[0])  # Assurez-vous que create_match prend les bons arguments
+            round_matches.append(match)
+            current_round.add_match(match)  # Ajouter le match à l'objet Round
 
-    return current_round  # Retourner l'objet Round complet avec les matchs
+            # Mise à jour du score
+            player1[1] += match.match_info[0][1]
+            player2[1] += match.match_info[1][1]
+
+            # Mise à jour des opposants
+            player1[2].append(player2)
+            player2[2].append(player1)
+
+        return current_round  # Retourner l'objet Round complet avec les matchs
