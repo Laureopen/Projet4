5e9cee241efc36cd48d32e15c72a2e789632da51
Laureopen <laurebretagne56@orange.fr>
2024-12-12 14:08:33 +0100

    Mise à jour

--- views/tournament_view.py
+++ views/tournament_view.py
@@ -1,35 +1,29 @@
 from datetime import datetime
 
-from models.tournament import Tournament
+
 
 class TournamentView:
+    def __init__(self, tournament=None):
+        self.tournament = tournament
+
     @staticmethod
     def show_generic_error(message):
         """Affiche les messages d'erreur."""
-        print(f"Error: {message}")
+        print(f"Erreur : {message}")
+
+
+    def prompt_for_tournament_creation(self):
+        """Demande les informations pour créer un tournoi."""
+        tournament_info = {}
+        tournament_info["name"] = input("Nom du tournoi : ")
+        tournament_info["location"] = input("Lieu : ")
+        tournament_info["start_date"] = input("Date de début (YYYY-MM-DD) : ")
+        tournament_info["end_date"] = input("Date de fin (YYYY-MM-DD) : ")
+
+        return tournament_info
 
     @staticmethod
-    def prompt_for_tournament_creation():
-        """Demande les informations pour créer un tournoi."""
-        name = input("Nom du tournoi: ")
-        location = input("Lieu: ")
-        start_date = input("Date de début (YYYY-MM-DD): ")
-        end_date = input("Date de fin (YYYY-MM-DD): ")
-
-        return TournamentView().create_tournament(name, location, start_date, end_date)
-
-    def create_tournament(self, name, location, start_date, end_date):
-        """Crée un tournoi et gère les erreurs."""
-        try:
-            # Tentative de création du tournoi
-            tournament = Tournament(name, location, start_date, end_date)
-            print(f"Tournament '{name}' créer avec succès!")
-            return tournament
-        except Exception as e:
-            # Si une erreur se produit, l'afficher via la méthode show_generic_error
-            TournamentView.show_generic_error(str(e))  # Affiche l'erreur
-
-    def get_valid_date(self, prompt):
+    def get_valid_date(prompt):
         """Obtenir une date valide de l'utilisateur."""
         while True:
             date_input = input(prompt)
@@ -38,26 +32,13 @@ class TournamentView:
             except ValueError:
                 print("Erreur dans le format des dates, essayez à nouveau (YYYY-MM-DD).")
 
-    def start_tournament(self, tournaments):
-        print("\nListe des tournois disponibles :")
-        if not tournaments:
-            print("Aucun tournoi disponible.")
-            return None
 
-        for idx, tournament in enumerate(tournaments, 1):
-            print(f"{idx}. {tournament.name}")
-
-        # Vérifier que le choix de l'utilisateur est valide
-        choice = self.get_valid_choice(tournaments)
-        if choice is None:
-            return None
-        return tournaments[choice]
-
-    def get_valid_choice(self, tournaments):
-        """Vérifie que le choix de l'utilisateur est un nombre valide."""
+    @staticmethod
+    def get_valid_choice(tournaments):
+        """Vérifie que le choix de l'utilisateur est un numéro valide."""
         while True:
             try:
-                choice = int(input("Choisissez un tournoi à démarrer (numéro): ")) - 1
+                choice = int(input("Choisissez un tournoi à démarrer (numéro) : ")) - 1
                 if 0 <= choice < len(tournaments):
                     return choice
                 else:
@@ -65,13 +46,15 @@ class TournamentView:
             except ValueError:
                 print("Veuillez entrer un numéro valide.")
 
-    def show_tournament_results(self, tournament):
-        print(f"\nRésultats du tournoi '{tournament.name}':")
+    @staticmethod
+    def show_tournament_results(tournament):
+        """Affiche les résultats d'un tournoi."""
+        print(f"\nRésultats du tournoi '{tournament.name}' :")
         if not tournament.players:
             print("Aucun joueur n'a participé à ce tournoi.")
             return
         for player in tournament.players:
-            print(f"{player.first_name} {player.last_name}: {player.points} points")
+            print(f"{player.first_name} {player.last_name} : {player.points} points")
 
     def display_tournaments(self, tournaments):
         """Affiche la liste des tournois avec des informations importantes."""
@@ -79,12 +62,34 @@ class TournamentView:
             print("\nListe des tournois :")
             for idx, tournament in enumerate(tournaments, 1):
                 print(f"{idx}. {tournament.name}")
-                print(f"   Lieu: {tournament.location}")
-                print(f"   Date de début: {tournament.start_date.strftime('%Y-%m-%d')}")
-                print(f"   Date de fin: {tournament.end_date.strftime('%Y-%m-%d')}")
+                print(f"   Lieu : {tournament.location}")
+                print(f"   Date de début : {tournament.start_date.strftime('%Y-%m-%d')}")
+                print(f"   Date de fin : {tournament.end_date.strftime('%Y-%m-%d')}")
                 print("----------------------------------------")
         else:
             print("Aucun tournoi disponible.")
 
+    def get_players(self):
+        """Retourne les joueurs d'un tournoi."""
+        if self.tournament:
+            return self.tournament.players
+        else:
+            print("Aucun tournoi sélectionné.")
+            return []
 
+    @classmethod
+    def show_file_not_found_error(cls):
+        """Affiche un message lorsque le fichier est introuvable."""
+        print("Erreur : Le fichier des tournois n'a pas été trouvé. Assurez-vous qu'il existe ou initialisez-le.")
 
+    @classmethod
+    def show_json_decode_error(cls):
+        """Affiche un message lorsque le fichier JSON est mal formaté."""
+        print(
+            "Erreur : Le fichier JSON des tournois contient des données corrompues ou mal formatées. Veuillez vérifier sa syntaxe."
+        )
+
+    @classmethod
+    def show_message(cls, message):
+        """Affiche un message générique."""
+        print(message)
