76d872f17c99495d5fd4b08464f03e133758250c
Laureopen <laurebretagne56@orange.fr>
2024-12-15 14:59:38 +0100

    Mise à jour

--- models/round.py
+++ models/round.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
 class Round:
-    def __init__(self, round_number: int, start_date: str, end_date: str):
+    def __init__(self, round_number, start_date, end_date):
         """Initialise un nouveau tour avec les informations fournies."""
         self.round_number = round_number
-        self.start_date = datetime.strptime(start_date, "%Y-%m-%d")  # Format YYYY-MM-DD
-        self.end_date = datetime.strptime(end_date, "%Y-%m-%d")  # Format YYYY-MM-DD
+        self.start_date = start_date
+        self.end_date = end_date
         self.matches = []  # Liste des matchs dans ce tour
 
     def add_match(self, match):
