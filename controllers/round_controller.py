923f1fab43d7f1f5e948fd00fecc7e10104d43c9
Laureopen <laurebretagne56@orange.fr>
2024-12-22 15:09:06 +0100

    Mise à jour

--- controllers/round_controller.py
+++ controllers/round_controller.py
@@ -1,42 +1,41 @@
-# from controllers.match_controller import create_match
-
-from models.round import Round
-
 import random
 
-POSSIBLE_SCORES = (
-    (0, 1),
-    (1, 0),
-    (0.5, 0.5)
-)
+from controllers.match_controller import MatchController
+
+from models.round import Round
 
 
 class RoundController:
 
-    def create_round(self, tournament, players, round_num):
+    def create_round(self, tournament, round_date, players, round_num):
 
+        self.match_controller = MatchController()
         self.tournament = tournament
         self.round_num = round_num
-        # players = sorted(players, key=lambda x: x.score, reverse=True)  # Trier les joueurs par score
+        self.round_date = round_date
         round_matches = []
         available_players = players.copy()
 
+        if round_num == 0:
+            random.shuffle(available_players)
+        else:
+            print(self.tournament.player_scores)
+            players.sort(key=lambda player: self.tournament.player_scores[player.player_id])
+
         # Créer un objet Round
-        current_round = Round(self.round_num, self.tournament.start_date, self.tournament.end_date)
+        current_round = Round(self.round_num, self.round_date)
 
         # Création des matchs pour ce round
         while available_players:
             player1 = available_players.pop(0)
 
             for idx, candidate in enumerate(available_players):
-                couples = tuple(r[:2] for r in self.tournament.get_rounds())
-                if not tuple([player1.player_id, candidate.player_id]) in couples:
-                    score = self.play_round()
-                    self.tournament.add_round(tuple([player1.player_id, candidate.player_id, score]))
-                    break
+                match = self.match_controller.create_match(player1, candidate)
+                round_matches.append(match)
+                current_round.add_match(match)
+
+                self.tournament.player_scores[player1.player_id] += match.match_info[0][1]
+                self.tournament.player_scores[candidate.player_id] += match.match_info[1][1]
+        self.tournament.add_round(current_round)
 
         return current_round  # Retourner l'objet Round complet avec les matchs
-
-    def play_round(self):
-        score = POSSIBLE_SCORES[random.randint(0, 2)]
-        return score
