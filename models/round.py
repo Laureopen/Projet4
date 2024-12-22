39971b6ea55fd4c9521b58915f4aa9d6bcd04979
Laureopen <laurebretagne56@orange.fr>
2024-12-22 15:06:22 +0100

    Mise à jour

--- models/round.py
+++ models/round.py
@@ -1,16 +1,34 @@
 from datetime import datetime
 
+
 class Round:
-    def __init__(self, round_number, start_date, end_date):
+
+    def __init__(self, round_number, round_date):
         """Initialise un nouveau tour avec les informations fournies."""
         self.round_number = round_number
-        self.start_date = start_date
-        self.end_date = end_date
+        self.round_date = round_date
         self.matches = []  # Liste des matchs dans ce tour
 
     def add_match(self, match):
         """Ajoute un match à la liste des matchs de ce tour."""
         self.matches.append(match)
+
+    def to_dict(self):
+        data_matches = []
+        for match in self.matches:
+            data_matches.append(
+                {
+                    'player1': match.match_info[0][0].last_name,
+                    'player2': match.match_info[1][0].last_name,
+                    'score_player1': match.match_info[0][1],
+                    'score_player2': match.match_info[1][1]
+                }
+            )
+        return {
+            'id': self.round_number,
+            'date': self.round_date,
+            'matches': data_matches
+        }
 
     def __str__(self):
         """Retourne une représentation en chaîne du tour."""
