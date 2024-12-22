1d77da6c8f342dd03b3a16f97f4422e1a1c75027
Laureopen <laurebretagne56@orange.fr>
2024-12-22 15:04:55 +0100

    Mise à jour

--- views/round_view.py
+++ views/round_view.py
@@ -14,8 +14,10 @@ class RoundView:
     def display_match_details(self, match):
         """Affiche les détails d'un match particulier (joueurs, score, etc.)."""
         print(f"\nDétails du match :")
-        print(f"Joueur 1: {match.match_info[0][0].last_name} {match.match_info[0][0].first_name} | Score: {match.match_info[0][1]}")
-        print(f"Joueur 2: {match.match_info[1][0].last_name} {match.match_info[1][0].first_name} | Score: {match.match_info[1][1]}")
+        print(
+            f"Joueur 1: {match.match_info[0][0].last_name} {match.match_info[0][0].first_name} | Score: {match.match_info[0][1]}")
+        print(
+            f"Joueur 2: {match.match_info[1][0].last_name} {match.match_info[1][0].first_name} | Score: {match.match_info[1][1]}")
         winner = match.match_info[0][1] > match.match_info[1][1] and match.match_info[0][0] or match.match_info[1][0]
         print(f"Gagnant: {winner.last_name} {winner.first_name}")
 
@@ -32,9 +34,17 @@ class RoundView:
             score1 = match.match_info[0][1]
             score2 = match.match_info[1][1]
             winner = player1 if score1 > score2 else player2
-            print(f"Match {i}: {player1.last_name} {player1.first_name} {score1} - {score2} {player2.last_name} {player2.first_name} | Gagnant: {winner.last_name} {winner.first_name}")
+            print(
+                f"Match {i}: {player1.last_name} {player1.first_name} {score1} - {score2} {player2.last_name} {player2.first_name} | Gagnant: {winner.last_name} {winner.first_name}")
 
     def prompt_for_continue(self):
         """Demander à l'utilisateur s'il souhaite continuer."""
         choice = input("\nSouhaitez-vous continuer ? (O/N): ").lower()
         return choice != "n"
+
+    def prompt_for_round_creation(self):
+        """Demande les informations pour créer un round."""
+        round_info["start_date"] = input("Date de début (YYYY-MM-DD) : ")
+        round_info["end_date"] = input("Date de fin (YYYY-MM-DD) : ")
+
+        return tournament_info
