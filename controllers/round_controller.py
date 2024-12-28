e1e28caef4065610893a71244061a0275d797fe0
Laureopen <laurebretagne56@orange.fr>
2024-12-28 18:58:08 +0100

    Mise à jour

--- controllers/round_controller.py
+++ controllers/round_controller.py
@@ -1,41 +1,48 @@
 import random
 
 from controllers.match_controller import MatchController
-
 from models.round import Round
 
 
 class RoundController:
 
-    def create_round(self, tournament, round_date, players, round_num):
+
+    def create_round(self, tournament, players, round_num):
 
         self.match_controller = MatchController()
         self.tournament = tournament
         self.round_num = round_num
-        self.round_date = round_date
-        round_matches = []
-        available_players = players.copy()
 
-        if round_num == 0:
-            random.shuffle(available_players)
+        round_matches = []
+        if round_num == 'round1':
+            random.shuffle(tournament.players)
         else:
-            print(self.tournament.player_scores)
-            players.sort(key=lambda player: self.tournament.player_scores[player.player_id])
+            tournament.players.sort(key=lambda player: self.tournament.player_scores[player.player_id], reverse=True)
 
         # Créer un objet Round
-        current_round = Round(self.round_num, self.round_date)
+        current_round = Round(self.round_num)
 
-        # Création des matchs pour ce round
-        while available_players:
-            player1 = available_players.pop(0)
+        for i in range(0, len(available_players), 2):
+            player1 = available_players[i]
+            players_couple = (player1.player_id, available_players[i + 1].player_id)
+            if players_couple not in self.tournament.have_played:
+                player2 = available_players[i + 1] if i + 1 < len(players) else None  # Gérer un nombre impair de joueurs
+            else:
+                print(f"{round_num} : {player1.last_name} ({self.tournament.player_scores[player1.player_id]}) et {available_players[i + 1].last_name} ({self.tournament.player_scores[available_players[i + 1].player_id]}) ont déjà joué ensemble")
+                continue
 
-            for idx, candidate in enumerate(available_players):
-                match = self.match_controller.create_match(player1, candidate)
+            # dans la vue
+            confirm = input(f"{round_num} : {player1.last_name} ({self.tournament.player_scores[player1.player_id]}) VS {player2.last_name} ({self.tournament.player_scores[player2.player_id]}) O/N ?")
+
+            if confirm == "O":
+                match = self.match_controller.create_match(player1, player2)
                 round_matches.append(match)
                 current_round.add_match(match)
-
                 self.tournament.player_scores[player1.player_id] += match.match_info[0][1]
-                self.tournament.player_scores[candidate.player_id] += match.match_info[1][1]
+                self.tournament.player_scores[player2.player_id] += match.match_info[1][1]
+                self.tournament.have_played.append((player1.player_id, player2.player_id))
+            else:
+                continue
         self.tournament.add_round(current_round)
 
         return current_round  # Retourner l'objet Round complet avec les matchs
