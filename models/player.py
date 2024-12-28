2843b7f57d7ab86300a53fb3e6040889c256c0f5
Laureopen <laurebretagne56@orange.fr>
2024-12-28 18:55:27 +0100

    Mise à jour

--- models/player.py
+++ models/player.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
+
 class Player:
+
     def __init__(self, last_name, first_name, birth_date, player_id):
         self.last_name = last_name
         self.first_name = first_name
