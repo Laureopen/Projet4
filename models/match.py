eedadfc8e237fddc2be64ff8f731136aad3f1bb6
Laureopen <laurebretagne56@orange.fr>
2024-12-28 18:56:06 +0100

    Mise à jour

--- models/match.py
+++ models/match.py
@@ -1,22 +1,24 @@
 from models.player import Player
 
+
 class Match:
-    def __init__(self, player1: Player, score_player1: int, player2: Player, score_player2: int):
+
+    def __init__(self, player1, score_player1, player2, score_player2):
         self.match_info = (
             [player1, score_player1],
             [player2, score_player2]
         )
 
-    def to_list(self):
-        return self.match_info
+    # def to_list(self):
+    #     return self.match_info
 
-    @staticmethod
-    def from_list(data):
-        player1 = data[0][0]
-        score_player1 = data[0][1]
-        player2 = data[1][0]
-        score_player2 = data[1][1]
-        return Match(player1, score_player1, player2, score_player2)
+    # @staticmethod
+    # def from_list(data):
+    #     player1 = data[0][0]
+    #     score_player1 = data[0][1]
+    #     player2 = data[1][0]
+    #     score_player2 = data[1][1]
+    #     return Match(player1, score_player1, player2, score_player2)
 
     def __str__(self):
         return f"{self.match_info[0][0].last_name} vs {self.match_info[1][0].last_name} | Scores: {self.match_info[0][1]} - {self.match_info[1][1]}"
