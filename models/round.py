63886d8e2a7d3e232e740401323c9ecaf98e0fa5
Laureopen <laurebretagne56@orange.fr>
2024-12-29 10:30:38 +0100

    Mise à jour

--- models/round.py
+++ models/round.py
@@ -3,10 +3,11 @@ from datetime import datetime
 
 class Round:
 
-    def __init__(self, round_number, round_date=None):
+    def __init__(self, round_number, round_start_date=None, round_end_date=None):
         """Initialise un nouveau tour avec les informations fournies."""
         self.round_number = round_number
-        self.round_date = round_date
+        self.round_start_date = round_start_date
+        self.round_end_date = round_end_date
         self.matches = []  # Liste des matchs dans ce tour
 
     def add_match(self, match):
@@ -18,15 +19,17 @@ class Round:
         for match in self.matches:
             data_matches.append(
                 {
-                    'player1': match.match_info[0][0].last_name,
-                    'player2': match.match_info[1][0].last_name,
-                    'score_player1': match.match_info[0][1],
-                    'score_player2': match.match_info[1][1]
+                    'player1': match.match_info[0][0].player_id,
+                    'player2': match.match_info[1][0].player_id,
+                    'scores': [
+                        match.match_info[0][1], match.match_info[1][1]
+                    ]
                 }
             )
         return {
             'id': self.round_number,
-            'date': self.round_date,
+            'round_start_date': self.round_start_date,
+            'round_end_date': self.round_end_date,
             'matches': data_matches
         }
 
@@ -34,5 +37,5 @@ class Round:
         """Retourne une représentation en chaîne du tour."""
         match_info = '\n'.join([f"Match {i+1}: {match}" for i, match in enumerate(self.matches)])
         return (f"Round {self.round_number}\n"
-                f"Start Date: {self.start_date.strftime('%d-%m-%Y')}, End Date: {self.end_date.strftime('%d-%m-%Y')}\n"
+                f"Start Date: {self.round_start_date.strftime('%d-%m-%Y')}, End Date: {self.round_end_date.strftime('%d-%m-%Y')}\n"
                 f"Matches: \n{match_info}")
