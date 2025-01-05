1667dc09e5c1dfbb4bc0a52ed8d4726fe3877e31
Laureopen <laurebretagne56@orange.fr>
2025-01-05 18:57:40 +0100

    Mise à jour

--- models/match.py
+++ models/match.py
@@ -8,29 +8,12 @@ class Match:
         self.player2 = player2
         self.match_info = []
 
-    def add_score(self, player1, score_player1, player2, score_player2):
+    def add_score(self, score_player1, score_player2):
         self.match_info = (
             [self.player1, score_player1],
             [self.player2, score_player2]
         )
 
-    # def to_list(self):
-    #     return self.match_info
-
-    # @staticmethod
-    # def from_list(data):
-    #     player1 = data[0][0]
-    #     score_player1 = data[0][1]
-    #     player2 = data[1][0]
-    #     score_player2 = data[1][1]
-    #     return Match(player1, score_player1, player2, score_player2)
-
     def __str__(self):
         return f"{self.player1.last_name} vs {self.player2.last_name}"
 
-    def get_winner(self):
-        if self.match_info[0][1] > self.match_info[1][1]:
-            return self.match_info[0][0]
-        elif self.match_info[0][1] < self.match_info[1][1]:
-            return self.match_info[1][0]
-        return None
