06e34f10052d81ec0a01bbe5954fe19790f0f439
Laureopen <laurebretagne56@orange.fr>
2025-01-05 10:21:26 +0100

    Mise à jour

--- models/match.py
+++ models/match.py
@@ -3,10 +3,15 @@ from models.player import Player
 
 class Match:
 
-    def __init__(self, player1, score_player1, player2, score_player2):
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        self.match_info = []
+
+    def add_score(self, player1, score_player1, player2, score_player2):
         self.match_info = (
-            [player1, score_player1],
-            [player2, score_player2]
+            [self.player1, score_player1],
+            [self.player2, score_player2]
         )
 
     # def to_list(self):
@@ -21,7 +26,7 @@ class Match:
     #     return Match(player1, score_player1, player2, score_player2)
 
     def __str__(self):
-        return f"{self.match_info[0][0].last_name} vs {self.match_info[1][0].last_name} | Scores: {self.match_info[0][1]} - {self.match_info[1][1]}"
+        return f"{self.player1.last_name} vs {self.player2.last_name}"
 
     def get_winner(self):
         if self.match_info[0][1] > self.match_info[1][1]:
