d73b1a37f125ee7872a1e035a9ffabc562e2e18b
Laureopen <laurebretagne56@orange.fr>
2025-01-05 19:02:20 +0100

    Mise à jour

--- controllers/match_controller.py
+++ controllers/match_controller.py
@@ -1,19 +1,17 @@
 import random
 from models.match import Match
-from views.match_view import MatchView
 from models.player import Player
 
 
 class MatchController:
 
-    def __init__(self):
-        self.match_view = MatchView()
+    def __init__(self, match=None):
+        self.match = match
 
-    def create_match(self, player1, player2):
+    @staticmethod
+    def create_match(player1, player2):
         match = Match(player1, player2)
         return match
 
-    def add_score(self, player1, player2, player1_score=None, player2_score=None):
-        self.match_view.display_match_creation(player1, player1_score, player2, player2_score)
-
-
+    def add_score_to_match(self, player1_score=None, player2_score=None):
+        self.match.add_score(player1_score, player2_score)
