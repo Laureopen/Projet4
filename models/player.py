ad3c675e1f2515387ab5d327ce277a93966ae86b
Laureopen <laurebretagne56@orange.fr>
2025-01-05 10:20:53 +0100

    Mise à jour

--- models/player.py
+++ models/player.py
@@ -12,7 +12,7 @@ class Player:
     def to_dict(self):
         """Retourne un dictionnaire pour le joueur."""
         return {
-            'player': self.last_name,
+            'last_name': self.last_name,
             'first_name': self.first_name,
             'birth_date': self.birth_date.strftime('%Y-%m-%d'),
             'player_id': self.player_id
