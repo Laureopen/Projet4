674f1f6d39e23d8b0be495518b202b7fd01579d9
Laureopen <laurebretagne56@orange.fr>
2025-01-05 10:23:56 +0100

    Mise à jour

--- controllers/round_controller.py
+++ controllers/round_controller.py
@@ -1,6 +1,7 @@
 import random
 
 from controllers.match_controller import MatchController
+from views.round_view import RoundView
 from datetime import datetime
 from models.round import Round
 
@@ -9,6 +10,7 @@ class RoundController:
 
     def __init__(self, tournament, players, round_num, start_date=None, end_date=None):
         self.match_controller = MatchController()
+        self.round_view = RoundView()
         self.tournament = tournament
         self.round_num = round_num
         self.players = players
@@ -16,13 +18,13 @@ class RoundController:
         self.end_date = end_date
 
     def create_round(self):
+        idx = 1
         round_matches = []
         available_players = self.players.copy()
         if self.round_num == 'round1':
             random.shuffle(available_players)
         else:
             available_players.sort(key=lambda player: self.tournament.player_scores[player.player_id], reverse=True)
-
         # Créer un objet Round
         current_round = Round(self.round_num, datetime.now().strftime('%Y-%m-%d'))
 
@@ -32,23 +34,32 @@ class RoundController:
             if players_couple not in self.tournament.have_played:
                 player2 = available_players[i + 1] if i + 1 < len(available_players) else None  # Gérer un nombre impair de joueurs
             else:
-                print(f"{self.round_num} : {player1.last_name} ({self.tournament.player_scores[player1.player_id]}) et {available_players[i + 1].last_name} ({self.tournament.player_scores[available_players[i + 1].player_id]}) ont déjà joué ensemble")
                 continue
+            match = self.match_controller.create_match(player1, player2)
 
-            # dans la vue
-            confirm = input(f"{self.round_num} : {player1.last_name} ({self.tournament.player_scores[player1.player_id]}) VS {player2.last_name} ({self.tournament.player_scores[player2.player_id]}) O/N ?")
+            current_round.add_match(match)
 
-            if confirm == "O":
-                match = self.match_controller.create_match(player1, player2)
-                round_matches.append(match)
-                current_round.add_match(match)
-                self.tournament.player_adversaries[player1.player_id].append(player2.player_id)
-                self.tournament.player_scores[player1.player_id] += match.match_info[0][1]
-                self.tournament.player_scores[player2.player_id] += match.match_info[1][1]
-                self.tournament.have_played.append((player1.player_id, player2.player_id))
-            else:
-                continue
-        current_round.round_end_date = datetime.now().strftime('%Y-%m-%d')
+            self.round_view.display_round_match(idx, self.tournament, player1, player2)
+            idx += 1
+
+
+            round_matches.append(match)
+
+
         self.tournament.add_round(current_round)
-
         return current_round  # Retourner l'objet Round complet avec les matchs
+
+    def start_round(self, round):
+        print(f"Veuillez jouer les parties, puis entrez les résultats :")
+        for idx, match in enumerate(round.matches):
+            score_player1 = input(f"Quel est le score de {match.player1.last_name} vs {match.player2.last_name} ? :")
+            if score_player1 not in ('0', '0.5', '1'):
+                print ("Erreur")
+            else:
+                self.tournament.player_adversaries[match.player1.player_id].append(match.player2.player_id)
+                self.tournament.player_scores[match.player1.player_id] += float(score_player1)
+                self.tournament.player_scores[match.player2.player_id] += 0 if float(score_player1) == 1 else 0.5 if float(score_player1) == 0.5 else 1
+                self.tournament.have_played.append((match.player1.player_id, match.player2.player_id))
+
+        # next_round = input("Voulez-vous lancer le round suivant ? (O/N) :")
+        round.round_end_date = datetime.now().strftime('%Y-%m-%d')
