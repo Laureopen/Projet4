79bbe8dc427991da44d8dc56eb493f62aea00dfd
Laureopen <laurebretagne56@orange.fr>
2025-01-12 17:28:06 +0100

    Mise à jour match.py

--- models/match.py
+++ models/match.py
@@ -1,19 +1,27 @@
-from models.player import Player
-
-
 class Match:
-
-    def __init__(self, player1, player2):
+    def __init__(self, player1, player2, score_1=0, score_2=0):
         self.player1 = player1
         self.player2 = player2
-        self.match_info = []
+        self.score_1 = score_1
+        self.score_2 = score_2
+        self.save_results()
 
-    def add_score(self, score_player1, score_player2):
-        self.match_info = (
-            [self.player1, score_player1],
-            [self.player2, score_player2]
+    def save_results(self):
+        # Sauvegarde des résultats sous forme de tuple avec les identifiants des joueurs et leurs scores
+        self.results = (
+            (self.player1.player_id, self.score_1),
+            (self.player2.player_id, self.score_2)
         )
 
-    def __str__(self):
-        return f"{self.player1.last_name} vs {self.player2.last_name}"
+    def get_scores(self):
+        # Retourne les résultats du match
+        return self.results
 
+    def update_result(self, score_1, score_2):
+        self.score_1 = score_1
+        self.score_2 = score_2
+        self.save_results()
+
+    def __str__(self):
+        # Affiche les informations du match de manière lisible
+        return f"{self.player1.name} ({self.score_1}) vs {self.player2.name} ({self.score_2})"
