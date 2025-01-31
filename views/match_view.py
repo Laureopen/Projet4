d888db4739f9a15b1e8578e06ba0faf5cfa4d8e4
Laureopen <laurebretagne56@orange.fr>
2025-01-31 18:48:13 +0100

    Mise à jour

--- views/match_view.py
+++ views/match_view.py
@@ -1,25 +1,22 @@
 class MatchView:
 
-    @staticmethod
-    def display_match_details(match):
+    def display_match_details(self, match):
         """
         Affiche les détails d'un match (joueurs, scores et gagnant).
 
         :param match: L'objet match contenant les informations du match.
         """
-        print("Détails du match :")  # Pas de f-string nécessaire ici
+        print("\nDétails du match :")
         player1, score1 = match.match_info[0]
         player2, score2 = match.match_info[1]
 
         print(f"Joueur 1: {player1.last_name} {player1.first_name} | Score: {score1}")
         print(f"Joueur 2: {player2.last_name} {player2.first_name} | Score: {score2}")
 
-        # Utilisation directe de la variable winner dans le print pour éviter l'erreur F841
-        print(f"Gagnant: {(player1 if score1 > score2 else player2).last_name} "
-              f"{(player1 if score1 > score2 else player2).first_name}")
+        winner = player1 if score1 > score2 else player2
+        print(f"Gagnant: {winner.last_name} {winner.first_name}")
 
-    @staticmethod
-    def display_match_result(winner):
+    def display_match_result(self, winner):
         """
         Affiche le résultat d'un match avec le gagnant ou un message de match nul.
 
@@ -30,8 +27,7 @@ class MatchView:
         else:
             print("Le match est un match nul.")
 
-    @staticmethod
-    def display_match_creation(player1, player2, score1, score2):
+    def display_match_creation(self, player1, player2, score1, score2):
         """
         Affiche un message indiquant qu'un match a été créé avec les joueurs et leurs scores.
 
